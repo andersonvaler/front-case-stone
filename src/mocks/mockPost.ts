a84d8c5479615a948cedaa2dfb06842842d3fb32
Anderson Valerio <anderson.valerio@picpay.com>
2023-01-26 02:18:12 -0300

    test(mocks): created mocks to implements unit tests

---
+++ src/mocks/mockPost.ts
@@ -0,0 +1,10 @@
+import { Post } from '../types';
+
+export const mockPost: Post = {
+    id: 1,
+    createdDate: 'Mock Date',
+    postPic: 'Mock Url',
+    createdByUserId: 1,
+    hashtags: [1],
+    likes: 100,
+};
