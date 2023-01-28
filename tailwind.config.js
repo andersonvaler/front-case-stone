d8f367fc07faeb8757abf17304a27e4212109781
Anderson Valerio <anderson.valerio@picpay.com>
2023-01-28 15:12:47 -0300

    feat(styles): adding screens configs to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                96: '55vh',
+            },
+        },
+        screens: {
+            sm: '680px',
+            md: '820px',
+            lg: '1180px',
+            xl: '1480px',
+            '2xl': '1736px',
+        },
     },
     plugins: [],
 };
