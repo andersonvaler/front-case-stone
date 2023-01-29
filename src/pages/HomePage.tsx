1dec147c2b1695cc61348691560f25c7306241ce
Anderson Valerio <anderson.valerio@picpay.com>
2023-01-29 12:20:20 -0300

    feat(home): adding alert component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
-import { PostsComponent, UserProfileComponent } from '../components';
+import { AlertComponent, PostsComponent, UserProfileComponent } from '../components';
+import { useAppState } from '../providers/AppState';
+import { AppStateEnum } from '../types';
 
-export function HomePage() {
+export const HomePage: React.FC = (): JSX.Element => {
+    const { appState } = useAppState();
+
     return (
         <div className="flex w-full flex-col sm:flex-row text-sm md:text-base">
             <div
@@ -13,6 +17,17 @@ export function HomePage() {
             <div className="flex flex-col items-center justfy-center w-full sm:overflow-y-scroll p-0 lg:px-16 h-screen">
                 <PostsComponent />
             </div>
+            {appState === AppStateEnum.ERROR && (
+                <AlertComponent type="error">Ops! Ocorreu um erro ao processar sua solicitação!</AlertComponent>
+            )}
+            {appState === AppStateEnum.SUCCESS && (
+                <AlertComponent type="success">Solicitação concluída com sucesso!</AlertComponent>
+            )}
+            {appState === AppStateEnum.LOADING && (
+                <div className=" w-screen h-screen bg-slate-50 absolute">
+                    <div className="loading-spinner"></div>
+                </div>
+            )}
         </div>
     );
-}
+};
